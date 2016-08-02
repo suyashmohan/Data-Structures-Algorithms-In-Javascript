50f6975843ca69ea4312d66cd906a7d94c10a212
Suyash Mohan <suyashatweb@gmail.com>
2016-08-02 18:39:41 +0530

    Comments added

--- Queue/Queue.js
+++ Queue/Queue.js
@@ -1,24 +1,28 @@
 'use strict';
 
+//Queue - FIFO
 const Queue = function(){
-  this.list = [];
-  this.length = 0;
+  this.list = []; //Array to hold items
+  this.length = 0; //Length of list
 
+  //Insert items At Rear
   this.insert = function(value){
-    this.list.push(value);
-    this.length += 1;
+    this.list.push(value); //Add item at End
+    this.length += 1; //Increase the length
     return this.list[this.length];
   }
 
+  //Delete Item from Front
   this.delete = function(){
-    if(this.length > 0){
-      let first = this.list[0];
-      this.list.splice(0, 1);
-      this.length -= 1;
+    if(this.length > 0){ //Check for Underflow Error`
+      let first = this.list[0]; //First Item
+      this.list.splice(0, 1); //Remvoe First Item
+      this.length -= 1; //Decrease the length
       return first;
     }
   }
 
+  //Traverse the list
   this.traverse = function(process){
     for(var i=0; i < this.length; i += 1){
       process(this.list[i]);
--- Stack/Stack.js
+++ Stack/Stack.js
@@ -1,24 +1,28 @@
 'use strict';
 
+//Stack Object - LIFO
 const Stack = function(){
-  this.list = [];
-  this.length = 0;
+  this.list = []; //Array to hold items
+  this.length = 0; //Count of items
 
+  //Push item into Array On Top
   this.push = function(value){
-    this.list.push(value);
-    this.length += 1;
+    this.list.push(value); //Add item at end of array
+    this.length += 1; //Increase length of List
     return this.list[this.length];
   }
 
+  //Delete item from Top
   this.pop = function(){
-    if(this.length > 0){
-      let last = this.list[this.list.length -1];
-      delete this.list[this.length -1];
-      this.length -= 1;
+    if(this.length > 0){ //Check for Underflow error
+      let last = this.list[this.list.length -1]; //Get Last item
+      delete this.list[this.length -1]; //Delete Last Item
+      this.length -= 1; //Decrease the length
       return last;
     }
   }
 
+  //traverse the list
   this.traverse = function(process){
     for(var i=this.length-1; i >= 0; i -= 1){
       process(this.list[i]);
